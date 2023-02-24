08a3b32dea253def0c823a97cb3dc489fe44650c
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2023-02-24 16:18:35 -0500

    Fixed incorrect merge resolve deletions.
    Co-authored-by: Incbom <IncbomDev@users.noreply.github.com>
    Co-authored-by: rohanmishra <rohanmishr@users.noreply.github.com>
    Co-authored-by: PillageDev <PillageDev@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,7 +18,7 @@ package frc.robot;
  * declared globally (i.e. public static). Do not put anything functional in
  * this class.
  *
- * <p>
+ * 
  * It is advised to statically import this class (or one of its inner classes)
  * wherever the
  * constants are needed, to reduce verbosity.
