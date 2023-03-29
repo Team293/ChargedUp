d0c722783b45ae7b50998b8c7f39f90ac0fe4ef1
LavaWaffle <bjxobjxo@gmail.com>
2023-03-29 18:20:04 -0400

    removed line 65

--- src/main/java/frc/robot/classes/SmoothControl.java
+++ src/main/java/frc/robot/classes/SmoothControl.java
@@ -62,7 +62,6 @@ public class SmoothControl {
 
   // Equation 14, Calculates the required turn rate
   private double calculateK(double range, double theta, double delta) {
-
     double retVal = -(1 / range) * (K2 * (delta - Math.atan(-K1 * theta)) +
         Math.sin(delta) * (1.0 + (K1 / (1.0 + Math.pow((K1 * theta), 2)))));
 
