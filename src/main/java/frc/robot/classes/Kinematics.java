2c751ca52aef780887eb69c411fe498454d65ef5
Paridax <tgavdaly@gmail.com>
2023-04-09 14:02:33 -0400

    Replace tabs with spaces

--- src/main/java/frc/robot/classes/Kinematics.java
+++ src/main/java/frc/robot/classes/Kinematics.java
@@ -7,151 +7,151 @@ import frc.robot.subsystems.Drivetrain;
  * encoder readings and current heading.
  */
 public class Kinematics {
-	private Position2D m_currentPose;
-	private double m_previousLeftEncoderPosition;
-	private double m_previousRightEncoderPosition;
-	private double m_trackWidthFeet = Drivetrain.TRACK_WIDTH_FEET;
+    private Position2D m_currentPose;
+    private double m_previousLeftEncoderPosition;
+    private double m_previousRightEncoderPosition;
+    private double m_trackWidthFeet = Drivetrain.TRACK_WIDTH_FEET;
 
-	public Kinematics(Position2D startingPosition) {
-		m_currentPose = startingPosition;
-		m_previousLeftEncoderPosition = 0;
-		m_previousRightEncoderPosition = 0;
-	}
+    public Kinematics(Position2D startingPosition) {
+        m_currentPose = startingPosition;
+        m_previousLeftEncoderPosition = 0;
+        m_previousRightEncoderPosition = 0;
+    }
 
-	/**
-	 * Calculates the current position of the robot based on encoder readings and
-	 * current heading.
-	 * @param leftEncoderPosition  The current position of the left encoder.
-	 * @param rightEncoderPosition The current position of the right encoder.
-	 * @param heading              The current heading of the robot.
-	 */
-	public void calculatePosition(double leftEncoderPostion, double rightEncoderPosition, double heading) {
-		double currentHeading = 0.0d;
-
-		updateCurrentPose(leftEncoderPostion, rightEncoderPosition);
-
-		// Limit psi between Pi and -Pi
-		currentHeading = limitRadians(heading);
-
-		// Calculate new attitude angle in radians
-		m_currentPose.setHeadingRadians(currentHeading);
-	}
-
-	/**
-	 * This function updates the robot's current pose based off of encoder distance traveled
-	 * @param leftEncoderPosition distance traveled by the left encoder
-	 * @param rightEncoderPosition distance traveled by the right encoder
-	 */
-	public void calculatePosition(double leftEncoderPostion, double rightEncoderPosition) {
-		updateCurrentPose(leftEncoderPostion, rightEncoderPosition);
-	}
-
-	/**
-	 * Updates the current pose of the robot based on the left and right encoder positions.
-	 * @param leftEncoderPosition the left encoder position in feet
-	 * @param rightEncoderPosition the right encoder position in feet
+    /**
+     * Calculates the current position of the robot based on encoder readings and
+     * current heading.
+     * @param leftEncoderPosition  The current position of the left encoder.
+     * @param rightEncoderPosition The current position of the right encoder.
+     * @param heading              The current heading of the robot.
      */
-	private void updateCurrentPose(double leftEncoderPostion, double rightEncoderPosition) {
-		double deltaLeft = 0.0d;
-		double deltaRight = 0.0d;
-		double deltaX = 0.0d;
-		double deltaY = 0.0d;
-		double deltaPsi = 0.0d;
-		double newPsi = 0.0d;
+    public void calculatePosition(double leftEncoderPostion, double rightEncoderPosition, double heading) {
+        double currentHeading = 0.0d;
 
-		// Calculate distance traveled
-		deltaLeft = (leftEncoderPostion - m_previousLeftEncoderPosition);
-		deltaRight = (rightEncoderPosition - m_previousRightEncoderPosition);
+        updateCurrentPose(leftEncoderPostion, rightEncoderPosition);
 
-		// Save distance traveled
-		m_previousLeftEncoderPosition = leftEncoderPostion;
-		m_previousRightEncoderPosition = rightEncoderPosition;
+        // Limit psi between Pi and -Pi
+        currentHeading = limitRadians(heading);
 
-		// Use encoders to calculate heading
-		deltaPsi = (deltaRight - deltaLeft) / m_trackWidthFeet; // this is the width from center left wheel to center
-																// right
-																// wheel
+        // Calculate new attitude angle in radians
+        m_currentPose.setHeadingRadians(currentHeading);
+    }
 
-		// Limit psi between Pi and -Pi
-		deltaPsi = limitRadians(deltaPsi);
-		newPsi = limitRadians(m_currentPose.getHeadingRadians() + deltaPsi);
+    /**
+     * This function updates the robot's current pose based off of encoder distance traveled
+     * @param leftEncoderPosition distance traveled by the left encoder
+     * @param rightEncoderPosition distance traveled by the right encoder
+     */
+    public void calculatePosition(double leftEncoderPostion, double rightEncoderPosition) {
+        updateCurrentPose(leftEncoderPostion, rightEncoderPosition);
+    }
 
-		// Calcualte new X, Y
-		deltaX = ((deltaLeft + deltaRight) / 2) * Math.cos(m_currentPose.getHeadingRadians()); // previous heading is in
-																								// radians
-		deltaY = ((deltaLeft + deltaRight) / 2) * Math.sin(m_currentPose.getHeadingRadians()); // previous heading is in
-																								// radians
+    /**
+     * Updates the current pose of the robot based on the left and right encoder positions.
+     * @param leftEncoderPosition the left encoder position in feet
+     * @param rightEncoderPosition the right encoder position in feet
+     */
+    private void updateCurrentPose(double leftEncoderPostion, double rightEncoderPosition) {
+        double deltaLeft = 0.0d;
+        double deltaRight = 0.0d;
+        double deltaX = 0.0d;
+        double deltaY = 0.0d;
+        double deltaPsi = 0.0d;
+        double newPsi = 0.0d;
 
-		// Update new position
-		m_currentPose.setX(m_currentPose.getX() + deltaX);
-		m_currentPose.setY(m_currentPose.getY() + deltaY);
-		m_currentPose.setHeadingRadians(newPsi);
-	}
+        // Calculate distance traveled
+        deltaLeft = (leftEncoderPostion - m_previousLeftEncoderPosition);
+        deltaRight = (rightEncoderPosition - m_previousRightEncoderPosition);
 
-	/**
-	 * Returns the current pose of the robot as a Position2D object, containing the
-	 * X, Y, and heading of the robot.
-	 * @return the current pose of the robot.
-	 */
-	public Position2D getPose() {
-		return m_currentPose;
-	}
+        // Save distance traveled
+        m_previousLeftEncoderPosition = leftEncoderPostion;
+        m_previousRightEncoderPosition = rightEncoderPosition;
 
-	/**
-	 * Sets the current pose of the robot.
-	 * @param pose the new pose of the robot.
-	 */
-	public void setPose(Position2D pose) {
-		// Reset Post
-		m_currentPose = pose;
-	}
+        // Use encoders to calculate heading
+        deltaPsi = (deltaRight - deltaLeft) / m_trackWidthFeet; // this is the width from center left wheel to center
+                                                                // right
+                                                                // wheel
 
-	/**
-	 * Limits the angle between -Pi and Pi.
-	 * @param radians the angle in radians.
-	 * @return the limited angle in radians.
-	 */
-	private double limitRadians(double radians) {
-		double retval = radians;
+        // Limit psi between Pi and -Pi
+        deltaPsi = limitRadians(deltaPsi);
+        newPsi = limitRadians(m_currentPose.getHeadingRadians() + deltaPsi);
 
-		while (retval > Math.PI) {
-			retval -= 2 * Math.PI;
-		}
+        // Calcualte new X, Y
+        deltaX = ((deltaLeft + deltaRight) / 2) * Math.cos(m_currentPose.getHeadingRadians()); // previous heading is in
+                                                                                                // radians
+        deltaY = ((deltaLeft + deltaRight) / 2) * Math.sin(m_currentPose.getHeadingRadians()); // previous heading is in
+                                                                                                // radians
 
-		while (retval < -Math.PI) {
-			retval += 2 * Math.PI;
-		}
+        // Update new position
+        m_currentPose.setX(m_currentPose.getX() + deltaX);
+        m_currentPose.setY(m_currentPose.getY() + deltaY);
+        m_currentPose.setHeadingRadians(newPsi);
+    }
 
-		return retval;
-	}
+    /**
+     * Returns the current pose of the robot as a Position2D object, containing the
+     * X, Y, and heading of the robot.
+     * @return the current pose of the robot.
+     */
+    public Position2D getPose() {
+        return m_currentPose;
+    }
 
-	/**
-	 * Calculates the distance between the current pose and a checkpoint.
-	 * @param checkpoint the checkpoint to calculate the distance to.
-	 * @return the distance to the checkpoint.
-	 */
-	public double distanceFromPose(Position2D checkpoint) {
-		double x1 = m_currentPose.getX();
-		double y1 = m_currentPose.getY();
-		double x2 = checkpoint.getX();
-		double y2 = checkpoint.getY();
-		double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    /**
+     * Sets the current pose of the robot.
+     * @param pose the new pose of the robot.
+     */
+    public void setPose(Position2D pose) {
+        // Reset Post
+        m_currentPose = pose;
+    }
 
-		return distance;
-	}
+    /**
+     * Limits the angle between -Pi and Pi.
+     * @param radians the angle in radians.
+     * @return the limited angle in radians.
+     */
+    private double limitRadians(double radians) {
+        double retval = radians;
 
-	/**
-	 * Checks if the robot is at a checkpoint.
-	 * @param checkpoint the checkpoint to check.
-	 * @param threshold the threshold distance to check if the robot is at the checkpoint.
-	 * @return true if the robot is at the checkpoint, false otherwise.
-	 */
-	public boolean atPose(Position2D checkpoint, double threshold) {
-		double distanceFromPose = distanceFromPose(checkpoint);
-		if (distanceFromPose <= threshold) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        while (retval > Math.PI) {
+            retval -= 2 * Math.PI;
+        }
+
+        while (retval < -Math.PI) {
+            retval += 2 * Math.PI;
+        }
+
+        return retval;
+    }
+
+    /**
+     * Calculates the distance between the current pose and a checkpoint.
+     * @param checkpoint the checkpoint to calculate the distance to.
+     * @return the distance to the checkpoint.
+     */
+    public double distanceFromPose(Position2D checkpoint) {
+        double x1 = m_currentPose.getX();
+        double y1 = m_currentPose.getY();
+        double x2 = checkpoint.getX();
+        double y2 = checkpoint.getY();
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        return distance;
+    }
+
+    /**
+     * Checks if the robot is at a checkpoint.
+     * @param checkpoint the checkpoint to check.
+     * @param threshold the threshold distance to check if the robot is at the checkpoint.
+     * @return true if the robot is at the checkpoint, false otherwise.
+     */
+    public boolean atPose(Position2D checkpoint, double threshold) {
+        double distanceFromPose = distanceFromPose(checkpoint);
+        if (distanceFromPose <= threshold) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
